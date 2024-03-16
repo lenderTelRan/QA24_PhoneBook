837efadc28af023d898e9aa359c8604006cc940f
lenderTelRan <lender.vladimir@gmail.com>
2024-03-16 14:56:35 +0200

    added positive login test

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,11 +1,32 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase {
 
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void openLoginRegistrationForm() {
+//        WebElement loginButton = wd.findElement(By.xpath("//a[text()='LOGIN']"));
+//        loginButton.click();
+        click(By.xpath("//a[text()='LOGIN']"));
+    }
+    public void fillLoginRegistrationForm(String email, String password) {
+//        WebElement emailInput = wd.findElement(By.cssSelector("input[name='email']"));
+//        WebElement passwordInput = wd.findElement(By.cssSelector("input[name='password']"));
 
+        type(By.cssSelector("input[name='email']"), email);
+        type(By.cssSelector("input[name='password']"), password);
+    }
+
+    public void submitLogin() {
+        click(By.name("login"));
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,17 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
+    String email = "test_telran@gmail.com";
+    String password = "Test@12345";
 
     @Test
-    public void test() {
-        
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(email, password);
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
